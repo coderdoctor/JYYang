558b94eb55bfb6363d7d5932e3a28e491ce7fe16
coderdoctor <coderyang@icloud.com>
2016-01-08 11:39:48 +0800

    第一次提交

---
+++ JYang/JYang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JYang
+//
+//  Created by kaicaibao on 16/1/8.
+//  Copyright © 2016年 kaicaibao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JYang/JYang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JYang
+//
+//  Created by kaicaibao on 16/1/8.
+//  Copyright © 2016年 kaicaibao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
